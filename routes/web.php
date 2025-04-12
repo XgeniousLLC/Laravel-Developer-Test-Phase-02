1d112e5746ca7831eca66fc5a6b1a00bd145afb0
Md Zahid <mdzahid.pro@gmail.com>
2025-04-13 00:39:31 +0600

    Laravel developer test setup done

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::get('/{page?}', function ($page = null) {
+    $page = $page ?? 'index.html';
+
+    $path = public_path("html/{$page}");
+
+    if (File::exists($path)) {
+        return response()->file($path);
+    }
+
+    abort(404);
+});
